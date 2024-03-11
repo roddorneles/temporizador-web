b1b01f9db8519181f4b61407aa86ca7c35e24ce9
roddorneles <rodrigodornelesferreira@usp.br>
2024-03-10 21:04:30 -0300

    Implementa funcao atualizaTempo

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 
+const labelTempo = document.querySelector('.label-tempo');
+
+const intervalId = setInterval( atualizaTempo, 10);
+
+console.log(intervalId);
+
+let tempo = 0;
+let tempoLimite = 0;
+
+function atualizaTempo(){
+
+    tempo = tempo + 10;
+    
+    const centesimoSegundo = (tempo/10)%100;
+    const segundo = Math.floor(tempo/1000)%60;
+    const minuto = Math.floor((tempo/1000)/60);
+
+    labelTempo.textContent = `${minuto}`.padStart(2, '0') + ` : ` + `${segundo}`.padStart(2,'0') + ` : ` + `${centesimoSegundo}`.padStart(2,0) ;
+    
+};
+
+function atingiuLimite(tempo){
+    return (tempo === tempoLimite);
+}
+
+
+
 
