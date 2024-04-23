fdc813d7f0c6f40286cd5b1d0e5f9cb85789a9c4
roddorneles <rodrigodornelesferreira@usp.br>
2024-04-23 15:53:34 -0300

    Finaliza primeira versão da pagina

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
 let tempoConfig = "0000";
-let tempoAcumulado = 0;
 
 const labelTempo = document.querySelector('.label-tempo');
 const labelTempoMinuto = document.querySelector('.label-minuto');
@@ -19,51 +18,46 @@ const botaoPause = document.querySelector('.btn-pause');
 const relogio = {
     intervalId: 0,
     tempoAtual: 0,
-    tempoInicio: 0,
     tempoLimite: 0,
-    tempoSalvo: 0,
     ultimaChamada: null,
     iniciou: false,
     modo: 'crono',
-    mudaModo: function(){
-        if (relogio.modo === 'crono'){
+    mudaModo: function () {
+        if (relogio.modo === 'crono') {
             relogio.modo = 'tempo';
         }
-        else if (relogio.modo === 'tempo'){
+        else if (relogio.modo === 'tempo') {
             relogio.modo = 'crono';
         }
         return relogio.modo;
     },
-    inicia: function (tempoInicio, tempoLimite){
-        
-        relogio.tempoSalvo = 0;
+    inicia: function (tempoInicio, tempoLimite) {
+
         relogio.tempoAtual = tempoInicio;
-        relogio.tempoInicio = tempoInicio;
         relogio.tempoLimite = tempoLimite;
 
         relogio.ultimaChamada = Date.now();
 
         relogio.iniciou = true;
-        relogio.intervalId = window.setInterval( relogio.atualizaTempo, 10);
+        relogio.intervalId = window.setInterval(relogio.atualizaTempo, 10);
 
     },
-    atualizaTempo: function (){       
+    atualizaTempo: function () {
 
-        const dt = Date.now() - relogio.ultimaChamada;       
+        const dt = Date.now() - relogio.ultimaChamada;
 
-        if (relogio.modo === 'crono'){
+        if (relogio.modo === 'crono') {
             relogio.tempoAtual = relogio.tempoAtual + dt;
         }
-        else if (relogio.modo === 'tempo'){
+        else if (relogio.modo === 'tempo') {
             relogio.tempoAtual = relogio.tempoAtual - dt;
-            //relogio.tempoAtual = relogio.tempoInicio - (relogio.tempoSalvo + dt);
         }
         relogio.ultimaChamada = Date.now();
 
-        if (relogio.modo ==='crono' && relogio.tempoAtual >= relogio.tempoLimite){
+        if (relogio.modo === 'crono' && relogio.tempoAtual >= relogio.tempoLimite) {
             relogio.interrompe();
         }
-        if (relogio.modo === 'tempo' && relogio.tempoAtual <= 0){
+        if (relogio.modo === 'tempo' && relogio.tempoAtual <= 0) {
             relogio.interrompe();
             return;
         }
@@ -72,35 +66,31 @@ const relogio = {
 
         relogio.formatarSegundos();
 
-        if (relogio.modo ==='crono' && Math.floor(relogio.tempoAtual/1000)*1000 >= relogio.tempoLimite){
+        if (relogio.modo === 'crono' && Math.floor(relogio.tempoAtual / 1000) * 1000 >= relogio.tempoLimite) {
             relogio.interrompe();
         }
 
-        //labelTempoMinuto.textContent = `${minuto}`.padStart(2, '0');
-        //labelTempoSegundo.textContent = `${segundo}`.padStart(2,'0');
-        //labelTempoCentSegundo.textContent = `${centesimoSegundo}`.padStart(2,0);
-        
     },
-    interrompe: function (){
+    interrompe: function () {
         window.clearInterval(relogio.intervalId);
         relogio.iniciou = false;
         botaoStart.textContent = "Start";
-        botaoStart.removeEventListener('click', clicaBotaoStop );
-        botaoStart.addEventListener('click', clicaBotaoStart);    
+        botaoStart.removeEventListener('click', clicaBotaoStop);
+        botaoStart.addEventListener('click', clicaBotaoStart);
     },
-    formatarSegundos: function(){
-        const centesimoSegundo = Math.floor((relogio.tempoAtual%1000)/10);
-        const segundo = Math.floor(relogio.tempoAtual/1000)%60;
-        const minuto = Math.floor((relogio.tempoAtual/1000)/60);
-    
-        labelTempo.textContent = `${minuto}`.padStart(2, '0') + ` : ` + `${segundo}`.padStart(2,'0') + ` : ` + `${centesimoSegundo}`.padStart(2,0) ;        
+    formatarSegundos: function () {
+        const centesimoSegundo = Math.floor((relogio.tempoAtual % 1000) / 10);
+        const segundo = Math.floor(relogio.tempoAtual / 1000) % 60;
+        const minuto = Math.floor((relogio.tempoAtual / 1000) / 60);
+
+        labelTempo.textContent = `${minuto}`.padStart(2, '0') + ` : ` + `${segundo}`.padStart(2, '0') + ` : ` + `${centesimoSegundo}`.padStart(2, 0);
     },
-    pause: function (){
+    pause: function () {
         window.clearInterval(relogio.intervalId);
     },
-    continua: function (){
+    continua: function () {
         relogio.ultimaChamada = Date.now();
-        relogio.intervalId = window.setInterval( relogio.atualizaTempo, 10);
+        relogio.intervalId = window.setInterval(relogio.atualizaTempo, 10);
     }
 };
 
@@ -110,42 +100,42 @@ botaoPause.addEventListener('click', clicaBotaoPause);
 
 botaoMudaModo.addEventListener('click', clicaBotaoMudaModo);
 
-botoesNumero.forEach( (botao) => {
+botoesNumero.forEach((botao) => {
     botao.addEventListener('click', apertaBotaoNumero);
 });
 
-botaoClean.addEventListener('click', function (){
-    if (relogio.iniciou){
+botaoClean.addEventListener('click', function () {
+    if (relogio.iniciou) {
         console.log("Tecla desabilitada");
         return;
-    }    
-    
+    }
+
     tempoConfig = "0000";
     labelConfiguracaoTempo.textContent = '00 : 00';
 });
 
-function clicaBotaoStart(){
+function clicaBotaoStart() {
     console.log("Start");
     let minutos = parseInt(tempoConfig.slice(0, 2));
-    let segundos = parseInt (tempoConfig.slice(2, 4));
+    let segundos = parseInt(tempoConfig.slice(2, 4));
 
-    if (minutos > 59){
+    if (minutos > 59) {
         minutos = 59;
     }
-    if (segundos > 59){
+    if (segundos > 59) {
         segundos = 59;
     }
 
     const tempoConfiguradoEmMs = minutos * 60 * 1000 + segundos * 1000;
 
-    if (tempoConfiguradoEmMs === 0){
+    if (tempoConfiguradoEmMs === 0) {
         return;
     }
 
-    if (relogio.modo === 'crono'){
+    if (relogio.modo === 'crono') {
         relogio.inicia(0, tempoConfiguradoEmMs);
     }
-    else if(relogio.modo === 'tempo'){
+    else if (relogio.modo === 'tempo') {
         relogio.inicia(tempoConfiguradoEmMs, 0);
     }
 
@@ -154,17 +144,17 @@ function clicaBotaoStart(){
     botaoStart.addEventListener('click', clicaBotaoStop);
 }
 
-function clicaBotaoStop(){
+function clicaBotaoStop() {
     console.log("Stop");
     relogio.interrompe();
 
     botaoPause.textContent = "Pause";
     botaoPause.removeEventListener('click', clicaBotaoRun);
-    botaoPause.addEventListener('click', clicaBotaoPause);            
+    botaoPause.addEventListener('click', clicaBotaoPause);
 }
 
-function clicaBotaoRun(){
-    if (!relogio.iniciou){
+function clicaBotaoRun() {
+    if (!relogio.iniciou) {
         console.log("Tecla desabilitada");
         return;
     }
@@ -175,8 +165,8 @@ function clicaBotaoRun(){
     botaoPause.addEventListener('click', clicaBotaoPause);
 }
 
-function clicaBotaoPause(){
-    if (!relogio.iniciou){
+function clicaBotaoPause() {
+    if (!relogio.iniciou) {
         console.log("Tecla desabilitada");
         return;
     }
@@ -187,28 +177,28 @@ function clicaBotaoPause(){
     botaoPause.addEventListener('click', clicaBotaoRun);
 }
 
-function clicaBotaoMudaModo(){
-    if (relogio.iniciou){
+function clicaBotaoMudaModo() {
+    if (relogio.iniciou) {
         console.log("Tecla desabilitada");
         return;
-    }    
+    }
     const novoModo = relogio.mudaModo();
-    if (novoModo === 'crono'){
+    if (novoModo === 'crono') {
         console.log("Crono");
         botaoMudaModo.textContent = 'CHRONOMETER';
     }
-    if (novoModo === 'tempo'){
+    if (novoModo === 'tempo') {
         console.log("Timer");
         botaoMudaModo.textContent = 'TIMER';
     }
 }
 
-function apertaBotaoNumero(event){
-    if (relogio.iniciou){
+function apertaBotaoNumero(event) {
+    if (relogio.iniciou) {
         console.log("Tecla desabilitada");
         return;
     }
-    
+
     const novoDigito = event.target.dataset['numero'];
 
     tempoConfig = tempoConfig + novoDigito;
@@ -217,9 +207,3 @@ function apertaBotaoNumero(event){
 
     labelConfiguracaoTempo.textContent = tempoConfig.slice(0, 2) + ` : ` + tempoConfig.slice(2, 4);
 }
-
-/*
-Olá, professor.
-
-Bom, para fazer o EP01 usei meus conhecimentos de HTML, CSS e JS que eu já tinha previamente. No entanto, na monitoria de hoje, percebi que o monitor usou Date.now() e AnimationFrame para mostrar como poderia ser feito pra solucionar o EP. Seria um problema eu entregar o EP sem usas essas funções e usando a função setInterval? Grato!
-*/
